94235aca13e2d33ca88273b0dd923a178371ecbd
GeeScot <github@gee.dev>
2022-02-22 18:53:13 +0000

    Split out clean/rimraf from build

--- src/services/express-service.ts
+++ src/services/express-service.ts
@@ -19,7 +19,7 @@ const createExpressService = () => {
   
   dynamic('../api', app);
 
-  const port = parseInt(process.env.PORT ?? '3000');
+  const port = process.env.PORT ?? 3000;
   app.listen(port, () => {
     log(`listening on port ${port}`);
   });
