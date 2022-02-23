96bfca34636242c72f7a63d3647948bf5e8d6cde
GeeScot <github@gee.dev>
2022-02-23 01:19:53 +0000

    Expanded to include team campaigns as well as individual

--- src/services/tiltify-service.ts
+++ src/services/tiltify-service.ts
@@ -11,34 +11,43 @@ const createTiltifyService = () => {
   });
 
   const getCampaign = async (userId: string, campaignSlug: string): Promise<any> => {
-    const targetUrl = `/api/v3/users/${userId}/campaigns/${campaignSlug}`;
+    const targetSubRoute = userId.startsWith('+') ? 'teams' : 'users';
+    const targetUserId = userId.startsWith('+') ? userId.substring(1) : userId;
+
+    const targetUrl = `/api/v3/${targetSubRoute}/${targetUserId}/campaigns/${campaignSlug}`;
     const { data: campaign } = await client.get(targetUrl);
 
     return campaign;
   };
 
   const getDonations = async (userId: string, campaignSlug: string): Promise<Donation[]> => {
-    const targetUrl = `/api/v3/users/${userId}/campaigns/${campaignSlug}`;
-    const { data: campaign } = await client.get(targetUrl);
+    const campaign = await getCampaign(userId, campaignSlug);
+    if (!campaign.data.endsAt) {
+      throw `Campaign ${campaign.name} hasn't ended yet`;
+    }
 
     const campaignEndDate = new Date(campaign.data.endsAt);
     if (campaignEndDate > new Date()) {
       throw `Campaign ${campaign.name} hasn't ended yet`;
     }
     
-    // TODO check if it's a team campaign and loop the below through all supporting campaigns and team campaign
+    const supportingCampaigns = await getSupportingCampaigns(campaign.data.id);
+    const supportingCampaignIds = supportingCampaigns.data.map((supportingCampaign: any): number => supportingCampaign.id);
+    supportingCampaignIds.push(campaign.data.id);
 
-    let donationsTargetUrl = `/api/v3/campaigns/${campaign.data.id}/donations?count=100`;
     let allDonations: Donation[] = [];
-    let donationCount = 0;
+    for (const supportingCampaignId of supportingCampaignIds) {
+      let donationsTargetUrl = `/api/v3/campaigns/${supportingCampaignId}/donations?count=100`;
+      let donationCount = 0;
 
-    do {
-      const { data: donations } = await client.get(donationsTargetUrl);
-      donationsTargetUrl = donations.links.prev;
+      do {
+        const { data: donations } = await client.get(donationsTargetUrl);
+        donationsTargetUrl = donations.links.prev;
 
-      donationCount = donations.data.length;
-      allDonations.push(...donations.data);
-    } while (donationCount === 100);
+        donationCount = donations.data.length;
+        allDonations.push(...donations.data);
+      } while (donationCount === 100);
+    }
 
     allDonations = allDonations.map((donation) => {
       return {
@@ -50,6 +59,13 @@ const createTiltifyService = () => {
 
     return allDonations;
   };
+
+  const getSupportingCampaigns = async (campaignId: number): Promise<any> => {
+    const targetUrl = `/api/v3/campaigns/${campaignId}/supporting-campaigns`;
+    const { data: supportingCampaigns } = await client.get(targetUrl);
+
+    return supportingCampaigns;
+  }
 
   return {
     getCampaign,
